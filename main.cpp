45e78c2f641bb2b7a868588ec2e5b8c19354ff4d
John Doe <122407541+Lukehamling@users.noreply.github.com>
2025-02-06 12:25:40 -0700

    Check if startup

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QCoreApplication>
+#include <QStringList>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QStringList arguments = QCoreApplication::arguments();
+    // TODO: We need to add this argument to startup locations. i.e. add to windows registery with exe location.
+    bool isStartup = arguments.contains("--startup");
+
+    if (isStartup) {
+        QCoreApplication a(argc, argv); // Core application runs minimal - may not be needed
+        qDebug() << "App started from system startup.";
+        // Run deamon, no visuals
+
+        return a.exec();
+    } else {
+        QApplication a(argc, argv);
+        qDebug() << "App started manually.";
+        // Run UI
+        MainWindow w;
+        w.show();
+
+        return a.exec();
+    }
 }
