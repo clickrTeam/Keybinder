d7e262aeb6b79319822be6c6d0dfd4e19da8412f
Ryan Dalrymple <ryan.d235711@gmail.com>
2025-02-23 18:21:27 -0700

    Correctly identified keyboard device and read inputs from the keyboard

--- ubuntu/learning/Detection.cpp
+++ ubuntu/learning/Detection.cpp
@@ -22,6 +22,9 @@ int main()
 
     struct dirent *entry;
     vector<string> possible_keyb_paths;
+    vector<int> fds;
+    vector<libevdev*> devices;
+
     int event_counter = 0;
     int possible_keyboards = 0;
 
@@ -37,15 +40,15 @@ int main()
 
             struct libevdev *dev = nullptr;
 
-            int fd = open(event_path.c_str(), O_RDONLY);
+            int fd = open(event_path.c_str(), O_RDONLY | O_NONBLOCK);
             if (fd < 0) {
-                std::cerr << "Failed to open " << event_path << std::endl;
+                cerr << "Failed to open " << event_path << endl;
                 return false;
             }
         
             // Initialize the evdev device
             if (libevdev_new_from_fd(fd, &dev) < 0) {
-                std::cerr << "Failed to initialize evdev device" << std::endl;
+                cerr << "Failed to initialize evdev device" << endl;
                 close(fd);
                 return false;
             }
@@ -57,14 +60,100 @@ int main()
                 possible_keyb_paths.push_back(event_path);
                 cout << "possible keyb path: " + event_path << endl;  // Still need to verify that this is actually a keyboard
                 possible_keyboards++;
+                fds.push_back(fd);
+                devices.push_back(dev);
             }
-            // Clean up
-            libevdev_free(dev);
-            close(fd);
+            else
+            {
+                // Clean up
+                libevdev_free(dev);
+                close(fd);
+            }
         }
     }
     // On my desktop, I get 5 devices. On my laptop, I only get 1 and it is the correct device.
     cout << "Number of /dev/input/eventX devices: " << event_counter << endl;
     cout << "Number of possible keyboards: " << possible_keyboards << endl;
+
+    // Now we listen to key events to determine which device is indeed the keyboard
+    cout << "Press SPACEBAR to identify the correct keyboard device." << endl;
+    bool found_keyb = false;
+    string keyb_path = "";
+    
+    while(!found_keyb)
+    {
+        for(size_t i = 0; i < devices.size(); i++)
+        {
+            struct input_event event;
+            while (libevdev_next_event(devices[i], LIBEVDEV_READ_FLAG_NORMAL, &event) == 0) 
+            {
+                if (event.type == EV_KEY && event.code == KEY_SPACE && event.value == 1) // Space has been pressed on devices[i]
+                {
+                    cout << "Detected spacebar press on: " << possible_keyb_paths[i] << endl;
+                    keyb_path = possible_keyb_paths[i];
+                    found_keyb = true;
+                    break;
+                }
+            }
+        }
+
+        if (found_keyb)
+        {
+            break;
+        }
+    }
+
+    // Clean up all devices
+    for(size_t i = 0; i < devices.size(); i++)
+    {
+        libevdev_free(devices[i]);
+        close(fds[i]);
+    }
+    devices.clear();
+    fds.clear();
+
+
+    // Re-open keyb now that we know proper device. It must be reopened because of the library.
+    int keyb_fd = open(keyb_path.c_str(), O_RDONLY | O_NONBLOCK);
+    if (keyb_fd < 0) 
+    {
+        cerr << "Failed to open device: " << strerror(errno) << endl;
+        return 1;
+    }
+
+    struct libevdev *keyb;
+    if (libevdev_new_from_fd(keyb_fd, &keyb) < 0) 
+    {
+        std::cerr << "Failed to initialize libevdev" << std::endl;
+        return 1;
+    }
+
+    // Detect other key presses
+    bool escape_pressed = false;
+    cout << "\nMonitoring key presses. Press ESC to terminate." << endl;
+
+    while(!escape_pressed)
+    {
+        struct input_event event;
+
+        while (libevdev_next_event(keyb, LIBEVDEV_READ_FLAG_NORMAL, &event) == 0) 
+        {
+            if (event.type == EV_KEY &&  event.value == 1) // ESC has been pressed
+            {
+                cout << "User pressed key: " << libevdev_event_code_get_name(EV_KEY, event.code) << endl;
+
+                if(event.code == KEY_ESC)
+                {
+                    escape_pressed = true;
+                    break;
+                }
+            }
+        }
+
+        if(escape_pressed)
+        {
+            break;
+        }
+    }
     return 0;
 }
