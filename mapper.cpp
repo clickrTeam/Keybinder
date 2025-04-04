ae17d9d707492041d80ac62fff50d5bb3d17a41d
Luke Hamling <122407541+Lukehamling@users.noreply.github.com>
2025-04-03 18:52:20 -0600

    Start mapping timed keys, still needs kybnd signleshot

--- mapper.cpp
+++ mapper.cpp
@@ -1,4 +1,5 @@
 #include "mapper.h"
+#include <QTimer>
 #ifdef _WIN32
 #include "win/deamon.h"
 #elif defined(__APPLE__)
@@ -18,6 +19,9 @@ void setProfile(Profile loaded) {
     qDebug() << activeLayer.tapKeyBinds.keys();
 }
 
+QMap<int, int> timedKeyProgress;
+int next_key;
+int first_key;
 
 bool mapKeyDownToBind(int virtualKey) {
     qDebug() << virtualKey;
@@ -26,6 +30,38 @@ bool mapKeyDownToBind(int virtualKey) {
         qDebug() << "Tap";
         press(activeLayer.tapKeyBinds[virtualKey]);
         return true;
+    }
+    else if (activeLayer.timedKeyBinds.contains(virtualKey) || next_key == virtualKey) {
+        if (activeLayer.timedKeyBinds.contains(virtualKey))
+            first_key = virtualKey;
+        else if (next_key == virtualKey) {
+            virtualKey = first_key;
+        }
+        TimedKeyBind kybnd = activeLayer.timedKeyBinds[virtualKey];
+
+        if (timedKeyProgress.contains(virtualKey)) {
+            timedKeyProgress[virtualKey]++;
+        } else {
+            timedKeyProgress[virtualKey] = 1;
+        }
+        if (timedKeyProgress[virtualKey] == kybnd.keyTimePairs.count()) {
+            // last keybind activate
+            press(kybnd.bind);
+            timedKeyProgress[virtualKey] = 0;
+            return true;
+        } else {
+            next_key = kybnd.keyTimePairs[timedKeyProgress[virtualKey]].keyValue;
+        }
+
+        if (kybnd.capture && kybnd.release) {
+            QTimer::singleShot(100, [arg1, arg2]() {
+                delayedFunction(arg1, arg2);
+            });
+        } else if (kybnd.capture) {
+            return true;
+        } else if (kybnd.release) {
+            return false;
+        }
     }
     return false;
 }
