340584807b225afe94dec1f5a2f37d6ac773400e
Luke Hamling <122407541+Lukehamling@users.noreply.github.com>
2025-04-04 09:21:23 -0600

    Comments and changes to make it more understandable.

--- mapper.cpp
+++ mapper.cpp
@@ -20,23 +20,29 @@ void setProfile(Profile loaded) {
 }
 
 // Timed varibles
-QMap<int, int> timedKeyProgress;
-int next_key;
-int first_key;
+QMap<int, int> timedKeyProgress; // represents progress towards finish the timed macro.
+int next_key; // the next expected key in a timed macro. Never includes the first.
+int first_key; // the first key in a expected macro.
 
-bool thenRelease;
-int capture_and_release_key;
+bool thenRelease; // true if some key is captured to release.
+int capture_and_release_key; // the key captured to release.
+
 
 bool mapKeyDownToBind(int virtualKey) {
     qDebug() << virtualKey;
 
     // Timed
-    if (activeLayer.timedKeyBinds.contains(virtualKey) || next_key == virtualKey) {
+    bool isAFirstKey = activeLayer.timedKeyBinds.contains(virtualKey);
+    if (isAFirstKey || next_key == virtualKey) {
         qDebug() << "Timed";
-        if (activeLayer.timedKeyBinds.contains(virtualKey) && first_key != virtualKey) // this goes second, to ensure a key can be mapped to mapped key
-            first_key = virtualKey;
+        if (next_key != virtualKey && isAFirstKey && first_key != virtualKey) {
+            // Starting a diffrent timed since firstKey is diffrent, and is not nextkey.
+            captureAndRelease(); // starting a diffrent timed
+            first_key = virtualKey; // this lets us use timed keys with other keys
+        }
         TimedKeyBind kybnd = activeLayer.timedKeyBinds[first_key];
 
+        // Progress
         if (timedKeyProgress.contains(first_key)) {
             qDebug() << "Incrementing Progress";
             timedKeyProgress[first_key]++;
@@ -50,7 +56,7 @@ bool mapKeyDownToBind(int virtualKey) {
             press(capture_and_release_key);
         }
 
-
+        // Finished proggress or setup next
         if (timedKeyProgress[first_key] == kybnd.keyTimePairs.count()) {
             qDebug() << "Finished Progress";
             thenRelease = false;
@@ -63,11 +69,13 @@ bool mapKeyDownToBind(int virtualKey) {
             next_key = kybnd.keyTimePairs[timedKeyProgress[first_key]].keyValue;
         }
 
+        // Capture and Release logic
         if (kybnd.capture && kybnd.release) {
             qDebug() << "Capture & Realeasing key";
             capture_and_release_key = virtualKey;
             thenRelease = true;
-            QTimer::singleShot(1000, []() {
+            int ms = kybnd.keyTimePairs[timedKeyProgress[first_key]].time;
+            QTimer::singleShot(ms, []() {
                 captureAndRelease();
             });
             return true;
@@ -80,7 +88,8 @@ bool mapKeyDownToBind(int virtualKey) {
         } else
             qCritical() << "FELL THROUGH CAPTURE AND RELEASE!" << kybnd.capture << kybnd.release;
     } else
-        captureAndRelease();
+        captureAndRelease(); // Cancel capture and release if bind is broken.
+
     // TAP
     if (activeLayer.tapKeyBinds.contains(virtualKey)) {
         qDebug() << "Tap";
@@ -90,16 +99,18 @@ bool mapKeyDownToBind(int virtualKey) {
     return false;
 }
 
+
 bool mapKeyUpToBind(int virtualKey) {
     return false;
     // TODO
 }
 
+
 void captureAndRelease() {
-    timedKeyProgress[first_key] = 0;
+    timedKeyProgress[first_key] = 0; // reset progress
     if (thenRelease) {
-        qDebug() << "Stopping CnR";
         thenRelease = false; // stop capture and release
-        press(capture_and_release_key);
+        press(capture_and_release_key); // release captured key
+        qDebug() << "Stopping CnR";
     }
 }
