6af4cf5c5f3cacc1916159314824570a991f8206
TimB44 <u1414110@umail.utah.edu>
2025-04-03 20:50:48 -0600

    Add stub for mac daemon

---
+++ mac/src/daemon.cpp
@@ -0,0 +1,13 @@
+#include "daemon.h"
+#include <iostream> // For demonstration purposes (could be replaced with logging, etc.)
+
+// TODO: fill this in
+Daemon::Daemon() { std::cout << "Daemon created" << std::endl; }
+
+Daemon::~Daemon() { std::cout << "Daemon destroyed" << std::endl; }
+
+void Daemon::start_daemon() { std::cout << "Daemon started" << std::endl; }
+
+void Daemon::cleanup() { std::cout << "Daemon cleaned up" << std::endl; }
+
+void Daemon::send_key() { std::cout << "Key sent" << std::endl; }
