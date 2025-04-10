6bb0f6e438469ed8296a2a7e09b5c8859e696b06
TimB44 <u1414110@umail.utah.edu>
2025-04-10 00:44:13 -0600

    Define callback for IOKit

--- mac/include/daemon.h
+++ mac/include/daemon.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "abstract_daemon.h"
+#include <CoreFoundation/CoreFoundation.h>
+#include <IOKit/hid/IOHIDManager.h>
+
 class Daemon : public AbstractDaemon {
   public:
     // Constructor and Destructor
@@ -10,4 +13,15 @@ class Daemon : public AbstractDaemon {
     void start() override;
     void cleanup() override;
     void send_key(int vk) override;
+
+  private:
+    IOHIDManagerRef hidManager;
+
+    // Callback for input events
+    static void input_event_callback(void *context, IOReturn result,
+                                     void *sender, IOHIDValueRef value);
+
+    // Callback for matching devices
+    static void device_matching_callback(void *context, IOReturn result,
+                                         void *sender, IOHIDDeviceRef device);
 };
