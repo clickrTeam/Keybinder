7376c1b570c348c915e6f020ec6be362b79b0ecb
TimB44 <u1414110@umail.utah.edu>
2025-04-19 00:14:53 -0600

    Add a few more properites to profiles

--- common/include/profile.h
+++ common/include/profile.h
@@ -23,16 +23,18 @@ enum class TimedBindBehavior {
     Default,
 };
 
+TimedBindBehavior parse_behavior(const QString &str);
+
 // Type alias would make it easier to change to an enum in the future if needed
 using KeyCode = int;
 
 struct KeyPress {
-    KeyCode keycode;
+    KeyCode key_code;
     static KeyPress from_json(const QJsonObject &obj);
 };
 
 struct KeyRelease {
-    KeyCode keycode;
+    KeyCode key_code;
     static KeyRelease from_json(const QJsonObject &obj);
 };
 
@@ -40,23 +42,22 @@ struct TapSequence {
     QList<KeyCode> key_sequence;
     TimedBindBehavior behavior;
     static TapSequence from_json(const QJsonObject &obj);
-    static TimedBindBehavior parseBehavior(const QString &str);
 };
 
 using Trigger = std::variant<KeyPress, KeyRelease, TapSequence>;
 
 struct PressKey {
-    KeyCode keycode;
+    KeyCode key_code;
     static PressKey from_json(const QJsonObject &obj);
 };
 
 struct ReleaseKey {
-    KeyCode keycode;
+    KeyCode key_code;
     static ReleaseKey from_json(const QJsonObject &obj);
 };
 
 struct TapKey {
-    KeyCode keycode;
+    KeyCode key_code;
     static TapKey from_json(const QJsonObject &obj);
 };
 
@@ -68,15 +69,16 @@ struct SwapLayer {
 using Bind = std::variant<PressKey, ReleaseKey, TapKey, SwapLayer>;
 
 struct Layer {
-  public:
-    QString layerName;
+    QString layer_name;
     QList<std::pair<Trigger, Bind>> remappings;
     static Layer from_json(const QJsonObject &obj);
 };
 
 struct Profile {
-  public:
     QString name;        // The name of the profile (e.g., "Default Profile")
     QList<Layer> layers; // List of layers in the profile
+    size_t default_layer;
     static Profile from_json(const QJsonObject &obj);
+    static Profile from_bytes(const QByteArray &bytes);
+    static Profile from_file(const QString &filename);
 };
