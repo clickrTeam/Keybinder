ccaf1a1dd8815d2874cc2564a53dffadb744fcdc
TimB44 <u1414110@umail.utah.edu>
2025-08-08 22:29:34 -0700

    Add key channel for listener mapper communication

---
+++ common/include/key_channel.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "event.h"
+#include <chrono>
+#include <condition_variable>
+#include <deque>
+#include <memory>
+#include <mutex>
+#include <optional>
+#include <utility>
+
+struct KeyQueueState {
+    std::deque<InputEvent> queue;
+    std::condition_variable cv;
+    std::mutex mtx;
+};
+
+// TODO: have some kind of filter to easily send keys back when they are not
+// being modified
+class KeySender {
+  public:
+    explicit KeySender(const std::shared_ptr<KeyQueueState> &state);
+    // Return true if the key was used. If false is returned then the key is not
+    // passed to the mapper
+    bool send_key(InputEvent kc);
+
+  private:
+    std::shared_ptr<KeyQueueState> state;
+};
+
+class KeyReceiver {
+  public:
+    explicit KeyReceiver(const std::shared_ptr<KeyQueueState> &state);
+
+    // Wait for a key with optional timeout
+    // If timeout is not provided, waits indefinitely
+    std::optional<InputEvent>
+    wait_key(std::optional<std::chrono::milliseconds> timeout = std::nullopt);
+
+  private:
+    std::shared_ptr<KeyQueueState> state;
+};
+
+// Factory to create a sender/receiver pair sharing the same queue
+std::pair<KeySender, KeyReceiver> create_channel();
