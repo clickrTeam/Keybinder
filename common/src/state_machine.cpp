6ccb78f31d66d9a82f4f61560347cab74e1216cb
TimB44 <u1414110@umail.utah.edu>
2025-09-15 14:26:16 -0600

    Define cpp file for state machine

---
+++ common/src/state_machine.cpp
@@ -0,0 +1,7 @@
+#pragma once
+#include "profile.h"
+#include "state_machine.h"
+#include <QVector>
+
+// Generates a vector of State objects from a given Layer
+QVector<State> generate_states(const Layer &layer) {}
