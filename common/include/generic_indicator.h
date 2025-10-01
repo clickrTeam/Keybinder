723657fdb5f383bad0ff70d355e62c42f5355933
Ryan Dalrymple <ryan.d235711@gmail.com>
2025-10-01 11:49:27 -0600

    added generic indicator files

---
+++ common/include/generic_indicator.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <QString>
+#include <QWidget>
+#include <QScreen>
+
+/**
+ * @brief The GenericIndicator class is a QWidget that will pop up to inform the user
+ * of something.
+ */
+class GenericIndicator : public QWidget {
+    Q_OBJECT
+  public:
+
+    /**
+     * @brief IndicatorPosition represents the position of the
+     * indicator on your screen.
+     */
+    typedef enum {
+        BOTTOM_RIGHT,
+        BOTTOM_LEFT,
+        TOP_RIGHT,
+        TOP_LEFT,
+        CENTER
+    } IndicatorPosition;
+
+    /**
+     * @brief GenericIndicator constructor for this class.
+     * @param note The string that will be displayed on the pop up.
+     * @param location The position on the screen where the indicator
+     * will pop up.
+     * @param duration_ms is the amount of time in milliseconds the pop up
+     * will be present on screen.
+     */
+    GenericIndicator(const QString &note, IndicatorPosition location, int duration_ms = 1000);
+
+  private:
+
+    /**
+     * @brief place_in_position Places the indicator in the desired location on the screen
+     * @param location The position that the indicator should be placed at.
+     * @param screen A pointer to the screen that the indicator should be placed on.
+     */
+    void place_in_position(IndicatorPosition location, QScreen *screen);
+};
