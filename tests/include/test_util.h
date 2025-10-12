a9ca8b8d2be43d560e34f84994b29dfd040e5aa3
TimB44 <u1414110@umail.utah.edu>
2025-10-12 12:08:47 -0600

    Add test helper functions

---
+++ tests/include/test_util.h
@@ -0,0 +1,7 @@
+#include "event.h"
+
+void mapper_test_timed(Profile p, QList<InputEvent> events,
+                       QList<uint64_t> waits, QList<OutputEvent> outputs);
+
+void mapper_test(Profile p, QList<InputEvent> events,
+                 QList<OutputEvent> outputs);
