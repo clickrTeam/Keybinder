674bd7a3da88bee23071135c2f581c5943456cce
TimB44 <u1414110@umail.utah.edu>
2025-10-12 13:12:40 -0600

    Add tostring methods for testing

--- common/include/event.h
+++ common/include/event.h
@@ -1,8 +1,9 @@
 #pragma once
-#include "event.h"
 #include "key_code.h"
 #include <QDebug>
 #include <QHash>
+#include <QString>
+#include <ostream>
 #include <profile.h>
 #include <variant>
 
@@ -28,3 +29,25 @@ inline uint qHash(const InputEvent &key, uint seed = 0) {
 // TODO: eventually these will become differnt however for now they can be the
 // same as we just handle keys
 using OutputEvent = std::variant<InputEvent, RunScript>;
+
+inline const char *to_string(KeyEventType t) {
+    switch (t) {
+    case KeyEventType::Press:
+        return "Press";
+    case KeyEventType::Release:
+        return "Release";
+    }
+}
+
+// Usefull for failing tests
+inline std::ostream &operator<<(std::ostream &os, const InputEvent &e) {
+    os << "InputEvent{ keycode=" << static_cast<int>(e.keycode)
+       << ", type=" << to_string(e.type) << " }";
+    return os;
+}
+
+// Pretty print OutputEvent (variant)
+inline std::ostream &operator<<(std::ostream &os, const OutputEvent &ev) {
+    std::visit([&](auto &&arg) { os << arg; }, ev);
+    return os;
+}
