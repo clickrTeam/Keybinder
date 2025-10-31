ba6861554d62e6971be0db3a9464e320eb47800c
Hayden <hayden.hilterbrand@gmail.com>
2025-10-31 07:42:37 -0600

    Implemented only letting one instance of keybinder run

--- common/src/main.cpp
+++ common/src/main.cpp
@@ -23,6 +23,10 @@
 #include <QThread>
 #include <QTimer>
 #include <csignal>
+#include <qcoreapplication.h>
+#include <qlogging.h>
+#include <qtmetamacros.h>
+#include <QLockFile>
 
 QThread *daemon_thread;
 QThread *mapper_thread;
@@ -48,6 +52,16 @@ void cleanShutdown() {
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+
+    QString lockFilePath = QDir::temp().absoluteFilePath("clickr.lock");
+    QLockFile lockFile(lockFilePath);
+    lockFile.setStaleLockTime(0); // optional: never consider stale locks valid
+
+    if (!lockFile.tryLock()) {
+        qCritical() << "Tried to run keybinder but another instance is already running.";
+        return 1;
+    }
+
     QString path = "empty";
     KeybinderSettings settings;
     KeyCounter key_counter;
@@ -117,6 +131,7 @@ int main(int argc, char *argv[]) {
                      &QCoreApplication::aboutToQuit,
                      [&logger]() { logger.cleanUp(); });
 
+    // Shutdown when the signal is sent
     Tray tray;
     QObject::connect(&tray, &Tray::shutdown, [&]() { cleanShutdown(); });
 
