1efdf7554e7e559b2bce4155058c85baae32da98
none <none>
2025-11-18 23:38:02 +0000

    fixup! Merge branch 'main' into 'tblamires-macos-keycodes'

--- win/include/daemon.h
+++ win/include/daemon.h
@@ -16,6 +16,7 @@ class Daemon : public AbstractDaemon {
     void cleanup() override;
     void send_outputs(const QList<OutputEvent> &vk) override;
     static LRESULT CALLBACK HookProc(int nCode, WPARAM wParam, LPARAM lParam);
-    static void CALLBACK WinEventProc(HWINEVENTHOOK hWinEventHook, DWORD event, HWND hwnd,
-                                      LONG idObject, LONG idChild, DWORD dwEventThread, DWORD dwmsEventTime);
+    static void CALLBACK WinEventProc(HWINEVENTHOOK hWinEventHook, DWORD event,
+                                      HWND hwnd, LONG idObject, LONG idChild,
+                                      DWORD dwEventThread, DWORD dwmsEventTime);
 };
